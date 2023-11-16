adc39a134b640b9586a05624c5dbea995a641662
rizqonm <birukucing72.gmail.com>
2023-11-16 20:25:40 +0700

    new file:   public/assets/login-bg.jpg 	modified:   resources/css/app.css 	modified:   resources/js/app.js 	modified:   resources/views/user/layout/main.blade.php 	modified:   resources/views/user/login.blade.php

---
+++ resources/views/user/layout/main.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login and Register Form</title>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+    <link rel="stylesheet" href="https://cdn.lineicons.com/4.0/lineicons.css">
+</head>
