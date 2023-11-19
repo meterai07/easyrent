20c5d5824595833766d52b74960d09ad28154272
madmasyhur2 <muhammadmasyhur2@gmail.com>
2023-11-19 11:28:01 +0700

    Transaction page

--- resources/views/transaction/page.blade.php
+++ resources/views/transaction/page.blade.php
@@ -1,29 +1,158 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends("transaction.layout.main")
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Transaction Page</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
-</head>
+@section('content')
 
-<body class="bg-gray-100">
-    <div class="container mx-auto" id="content">
-        <div class="flex justify-center items-center h-screen" id="Transaction-info">
-            <div class="bg-white rounded shadow p-6">
-                <div class="text-center">
-                    <h1 class="text-2xl font-bold mb-2">Transaction Info</h1>
-                    <p class="text-gray-600 mb-6">Please enter yout transaction info</p>
+<div class="flex justify-center" id="container">
+    <div class="mr-12">
+        {{-- Transaction Info --}}
+        <div class="flex flex-col justify-center items-center" id="Transaction-info">
+            <div class="bg-white rounded shadow p-6 m-4">
+                <div class="text-left">
+                    <h1 class="text-2xl font-bold mb-1">Transaction Info</h1>
+                    <p class="text-gray-600 mt-0 mb-8">Please enter yout transaction info</p>
                 </div>
-                <div class="text-center">
-                        
+                <div class="grid gap-x-12 gap-y-3 grid-cols-2 grid-rows-2">
+                    <div>
+                        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                        <input type="text" name="name" id="name" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Your Name">
+                    </div>
+                    <div>
+                        <label for="phone_number" class="block text-sm font-medium leading-6 text-gray-900">Phone Number</label>
+                        <input type="text" name="phone_number" id="phone_number" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="08XXXXXXXXX">
+                    </div>
+                    <div>
+                        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Address</label>
+                        <input type="text" name="address" id="address" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Your Address">
+                    </div>
+                    <div>
+                        <label for="City" class="block text-sm font-medium leading-6 text-gray-900">City</label>
+                        <input type="text" name="city" id="city" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="City">
+                    </div>
                 </div>
-                <div id="snap-container"></div>
             </div>
         </div>
+        {{-- Rental Info --}}
+        <div class="flex flex-col justify-center items-center" id="Rental-info">
+            <div class="bg-white rounded shadow p-6 m-4">
+                <div class="text-left">
+                    <h1 class="text-2xl font-bold mb-1">Rental Info</h1>
+                    <p class="text-gray-600 m-0">Please select your rental info</p>
+                </div>
+                <h1 class="text-2xl font-bold my-8">Pick Up Info</h1>
+                <div class="grid gap-x-12 gap-y-3 grid-cols-2 grid-rows-2">
+                    <div>
+                        <label for="pick_up_date" class="block text-sm font-medium leading-6 text-gray-900">Pick Up Date</label>
+                        <input type="date" name="pick_up_date" id="pick_up_date" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Date">
+                    </div>
+                    <div>
+                        <label for="pick_up_location" class="block text-sm font-medium leading-6 text-gray-900">Pick Up Location</label>
+                        <input type="text" name="pick_up_location" id="pick_up_location" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Location">
+                    </div>
+                    <div>
+                        <label for="pick_up_time" class="block text-sm font-medium leading-6 text-gray-900">Time</label>
+                        <input type="time" name="pick_up_time" id="pick_up_time" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Time">
+                    </div>
+                </div>
+                <h1 class="text-2xl font-bold my-8">Drop Off Info</h1>
+                <div class="grid gap-x-12 gap-y-3 grid-cols-2 grid-rows-2">
+                    <div>
+                        <label for="drop_off_date" class="block text-sm font-medium leading-6 text-gray-900">Drop off Date</label>
+                        <input type="date" name="drop_off_date" id="drop_off_date" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Date">
+                    </div>
+                    <div>
+                        <label for="Drop off Location" class="block text-sm font-medium leading-6 text-gray-900">Drop off Location</label>
+                        <input type="text" name="drop_off_location" id="drop_off_location" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Location">
+                    </div>
+                    <div>
+                        <label for="drop_off_time" class="block text-sm font-medium leading-6 text-gray-900">Time</label>
+                        <input type="time" name="drop_off_time" id="drop_off_time" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Time">
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- Payment Method --}}
+        <div class="flex flex-col justify-center" id="Payment-method">
+            <div class="bg-white rounded shadow p-6 m-4">
+                <div class="text-left">
+                    <h1 class="text-2xl font-bold mb-1">Payment Method</h1>
+                    <p class="text-gray-600 m-0">Please select your payment method</p>
+                </div>
+                <div>
+                    <div class="flex flex-col my-6">
+                        <div class="flex flex-row items-center mb-3">
+                            <label for="bca" class="w-full flex items-center cursor-pointer">
+                                <input type="radio" name="payment_method" id="bca" class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out mr-2">
+                                <span class="text-sm leading-5 font-medium text-gray-700">BCA</span>
+                                <img src="../assets/bca-logo.png" alt="BCA Logo" class="ml-auto h-6 w-auto">
+                            </label>
+                        </div>
+                        <div class="flex flex-row mb-3">
+                            <label for="bni" class="w-full flex items-center cursor-pointer">
+                                <input type="radio" name="payment_method" id="bni" class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out mr-2">
+                                <span class="text-sm leading-5 font-medium text-gray-700">BNI</span>
+                                <img src="../assets/bni-logo.png" alt="BNI Logo" class="ml-auto h-6 w-auto">
+                            </label>
+                        </div>
+                        <div class="flex flex-row mb-3">
+                            <label for="bri" class="w-full flex items-center cursor-pointer">
+                                <input type="radio" name="payment_method" id="bri" class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out mr-2">
+                                <span class="text-sm leading-5 font-medium text-gray-700">BRI</span>
+                                <img src="../assets/bri-logo.png" alt="BRI Logo" class="ml-auto h-6 w-auto">
+                            </label>
+                        </div>
+                        <div class="flex flex-row mb-3">
+                            <label for="bsi" class="w-full flex items-center cursor-pointer">
+                                <input type="radio" name="payment_method" id="bsi" class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out mr-2">
+                                <span class="text-sm leading-5 font-medium text-gray-700">BSI</span>
+                                <img src="../assets/bsi-logo.png" alt="BSI Logo" class="ml-auto h-6 w-auto">
+                            </label>
+                        </div>
+                        <div class="flex flex-row mb-3">
+                            <label for="mandiri" class="w-full flex items-center cursor-pointer">
+                                <input type="radio" name="payment_method" id="mandiri" class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out mr-2">
+                                <span class="text-sm leading-5 font-medium text-gray-700">Mandiri</span>
+                                <img src="../assets/mandiri-logo.png" alt="Mandiri Logo" class="ml-auto h-6 w-auto">
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="bg-indigo-600 hover:bg-indigo-700 text-white font-bold ml-4 mt-2 mb-8 py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            Pay Now
+        </button>
+    </div>
+{{-- Aside --}}
+    <div class="float-right ml-12">
+        <div class="flex flex-col justify-center items-center" id="aside">
+            <div class="bg-white rounded shadow p-6 m-4">
+                <div class="text-left">
+                    <h1 class="text-2xl font-bold mb-1">Mobil 1</h1>
+                    <p class="text mb-6">Sedan</p>
+                    <img src="../assets/item-product.png" alt="" width="auto" height="auto">
+                </div>
+                <h3 class="text-base font-bold my-6">Rent Summary</h3>
+                <div class="divide-y-2 divide-neutral-950">
+                    <div>
+                        <div class="grid gap-x-auto gap-y-3 grid-cols-2 grid-rows-3 my-6">
+                            <p class="text-sm text-left">Subtotal</p>
+                            <p class="text-sm text-right">Rp 500.000</p>
+                            <p class="text-sm text-left">Tax</p>
+                            <p class="text-sm text-right">Rp 0</p>
+                            <p class="text-sm text-left">Service fee</p>
+                            <p class="text-sm text-right">Rp 1.000</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="grid gap-x-auto gap-y-auto grid-cols-2 grid-rows-1 my-6">
+                            <p class="text-sm font-bold text-left">Total</p>
+                            <p class="text-sm font-bold text-right">Rp 501.000</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</body>
+</div>
 
-</html>
+@endsection
