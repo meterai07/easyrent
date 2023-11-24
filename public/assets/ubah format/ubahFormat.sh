adbe1c766edfcf44e2c2b95c3daec9434e31785d
rizqonm <birukucing72.gmail.com>
2023-11-24 21:22:37 +0700

    modified:   public/assets/ubah format/ubahFormat.sh 	new file:   resources/views/user/detail/page.blade.php

--- public/assets/ubah format/ubahFormat.sh
+++ public/assets/ubah format/ubahFormat.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# /mnt/d/kuliah/smt 5/rpl/proyek akhir/easyrent/public/assets/ubah format
+
 for tc in $(ls *); do
 	filename_without_extension=$(basename -- "$tc" | cut -f 1 -d '.')
 	cp $tc $filename_without_extension.jpg
