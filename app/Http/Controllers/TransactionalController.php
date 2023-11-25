54c90a860b09269a5c8068d0119d95beb531072f
Tengku rafi <t.mrr39@gmail.com>
2023-11-25 12:10:12 +0700

    feat: logic pembayaran sementara

--- app/Http/Controllers/TransactionalController.php
+++ app/Http/Controllers/TransactionalController.php
@@ -12,11 +12,12 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Requests\StoreTransactionalRequest;
 use App\Http\Requests\UpdateTransactionalRequest;
 use \Midtrans\Snap;
+use Midtrans\Transaction;
 
 class TransactionalController extends Controller
 {
     public function show(Request $request)
-    {   
+    {
         $vehicles = Vehicle::where('id', $request->id)->first();
         $categories = Category::where('id', $vehicles->category_id)->first();
 
@@ -26,49 +27,23 @@ class TransactionalController extends Controller
         ]);
     }
 
-    public function invoice(StoreTransactionalRequest $request)
-    {
-        $validated = $request->validated();
+    // public function invoice(StoreTransactionalRequest $request)
+    // {
+    //     $validatedData = $request->validated();
 
-        $user = auth()->user();
-        $tenantID = Tenant::where('user_id', $user->id)->first()->id;
+    //     // logic buat pembayaran total harga disini
+    //     $validatedData['totalPayment'] = $validatedData['total_payment'];
 
-        DB::transaction(function () use ($validated, $user, $tenantID) {
-            $transactional = Transactional::create([
-                'tenant_id' => $tenantID,
-                'vehicle_id' => $validated['vehicle_id'],
-                'rent_date' => $validated['rent_date'],
-                'rent_period' => $validated['rent_period'],
-                'total_payment' => $validated['total_payment'],
-            ]);
-
-            Config::$serverKey = config('midtrans.server_key');
-            Config::$isProduction = false;
-            Config::$isSanitized = true;
-            Config::$is3ds = true;
-
-            $params = array(
-                'transaction_details' => array(
-                    'order_id' => $transactional->id,
-                    'gross_amount' => $transactional->total_payment,
-                ),
-                'customer_details' => array(
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'phone' => $user->phone_number,
-                ),
-            );
-
-            $snapToken = \Midtrans\Snap::getSnapToken($params);
-            return view('invoice.page', compact('snapToken', 'transactional'));
-        });
-    }
+    //     return view('invoice.page', [
+    //         'validatedData' => $validatedData,
+    //     ]);
+    // }
 
     public function callback(Request $request)
     {
         $serverKey = config('midtrans.server_key');
-        $hashed = hash('sha512', $request->order_id.$request->status_code.$request->gross_amount.$serverKey);
-    
+        $hashed = hash('sha512', $request->order_id . $request->status_code . $request->gross_amount . $serverKey);
+
         if ($hashed == $request->signature_key) {
             if ($request->transaction_status == 'capture') {
                 $transactional = Transactional::findOrFail($request->order_id);
@@ -78,54 +53,52 @@ class TransactionalController extends Controller
         }
     }
 
-    // public function transaction(Request $request)
-    // {
-    //     dd($request->all());
-    // }
-
     public function transaction(StoreTransactionalRequest $request)
-    {   
+    {
         $validatedData = $request->validated();
 
-            $transaction = new Transactional([
-                'tenant_id' => $validatedData['tenant_id'],
-                'vehicle_id' => $validatedData['vehicle_id'],
-                'name' => $validatedData['name'],
-                'phone_number' => $validatedData['phone_number'],
-                'address' => $validatedData['address'],
-                'city' => $validatedData['city'],
-                'pick_up_date' => $validatedData['pick_up_date'],
-                'pick_up_location' => $validatedData['pick_up_location'],
-                'pick_up_time' => $validatedData['pick_up_time'],
-                'drop_off_date' => $validatedData['drop_off_date'],
-                'drop_off_location' => $validatedData['drop_off_location'],
-                'drop_off_time' => $validatedData['drop_off_time'],
-                'total_payment' => $validatedData['total_payment'], // total_payment = rent_period * vehicle_price
-                'payment_status' => "PROCESS",
-                'payment_method' => $validatedData['payment_method']
-            ]);
+        // logic buat pembayaran total harga disini
+        $totalPayment = $validatedData['total_payment'] * 1000;
 
-            $transaction->save();
+        $transaction = new Transactional([
+            'tenant_id' => $validatedData['tenant_id'],
+            'vehicle_id' => $validatedData['vehicle_id'],
+            'name' => $validatedData['name'],
+            'phone_number' => $validatedData['phone_number'],
+            'address' => $validatedData['address'],
+            'city' => $validatedData['city'],
+            'pick_up_date' => $validatedData['pick_up_date'],
+            'pick_up_location' => $validatedData['pick_up_location'],
+            'pick_up_time' => $validatedData['pick_up_time'],
+            'drop_off_date' => $validatedData['drop_off_date'],
+            'drop_off_location' => $validatedData['drop_off_location'],
+            'drop_off_time' => $validatedData['drop_off_time'],
+            'total_payment' => $totalPayment,
+            'payment_status' => "PROCESS",
+            'payment_method' => $validatedData['payment_method']
+        ]);
 
-            Config::$serverKey = config('midtrans.server_key');
-            Config::$isProduction = false;
-            Config::$isSanitized = true;
-            Config::$is3ds = true;
+        $transaction->save();
 
-            $params = array(
-                'transaction_details' => array(
-                    'order_id' => $transaction->id,
-                    'gross_amount' => $transaction->total_payment,
-                ),
-                'customer_details' => array(
-                    'name' => $transaction->name,
-                    // 'email' => $user->email,
-                    'phone' => $transaction->phone_number,
-                ),
-            );
+        Config::$serverKey = config('midtrans.server_key');
+        Config::$isProduction = false;
+        Config::$isSanitized = true;
+        Config::$is3ds = true;
 
-            $snapToken = Snap::getSnapToken($params);
-            return view('invoice.page', compact('snapToken', 'transaction'));
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => $transaction->id,
+                'gross_amount' => $transaction->total_payment,
+            ),
+            'customer_details' => array(
+                'name' => $transaction->name,
+                // 'email' => $user->email,
+                'phone' => $transaction->phone_number,
+            ),
+        );
+
+        $snapToken = Snap::getSnapToken($params);
+        return view('invoice.page', compact('snapToken', 'transaction'));
 
         // return redirect()->route('product')->with('success', 'Transaction saved successfully!');
     }
--- resources/views/invoice/page.blade.php
+++ resources/views/invoice/page.blade.php
@@ -4,71 +4,101 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Pembayaran</title>
+    <title>Confirmation Page</title>
     <script type="text/javascript"
     src="{{ config('midtrans.snap_url') }}"
     data-client-key="{{ config('midtrans.client_key') }}"></script>
 </head>
 <body>
-    <table>
-        <tr>
-            <td>Total Pembayaran</td>
-            <td> : {{ $transaction->total_payment }}</td>
-        </tr>
-    </table>
-    <button id="pay-button">Bayar Sekarang</button>
-
+        <div class="flex flex-col justify-center items-center" id="Transaction-info">
+            <div class="bg-white rounded shadow p-6 m-4">
+                <div class="grid gap-x-12 gap-y-3 grid-cols-2 grid-rows-2">
+                  <h1>{{ $transaction->total_payment }}</h1>
+                    <div>
+                        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                        <input disabled value="{{ $transaction->name }}" type="text" name="name" id="name" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="name">
+                    </div>
+                    <div>
+                        <label for="phone_number" class="block text-sm font-medium leading-6 text-gray-900">Phone Number</label>
+                        <input disabled value="{{ $transaction->phone_number }}" type="text" name="phone_number" id="phone_number" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="08XXXXXXXXX">
+                    </div>
+                    <div>
+                        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Address</label>
+                        <input disabled value="{{ $transaction->name }}" type="text" name="address" id="address" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Your Address">
+                    </div>
+                    <div>
+                        <label for="City" class="block text-sm font-medium leading-6 text-gray-900">City</label>
+                        <input disabled value="{{ $transaction->city }}" type="text" name="city" id="city" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="City">
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- Rental Info --}}
+        <div class="flex flex-col justify-center items-center" id="Rental-info">
+            <div class="bg-white rounded shadow p-6 m-4">
+                <h1 class="text-2xl font-bold my-8">Pick Up Info</h1>
+                <div class="grid gap-x-12 gap-y-3 grid-cols-2 grid-rows-2">
+                    <div>
+                        <label for="pick_up_date" class="block text-sm font-medium leading-6 text-gray-900">Pick Up Date</label>
+                        <input disabled value="{{ $transaction->pick_up_date}}" type="date" name="pick_up_date" id="pick_up_date" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Date">
+                    </div>
+                    <div>
+                        <label for="pick_up_location" class="block text-sm font-medium leading-6 text-gray-900">Pick Up Location</label>
+                        <input disabled value="{{ $transaction->pick_up_location }}" type="text"  name="pick_up_location" id="pick_up_location" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Location">
+                    </div>
+                    <div>
+                        <label for="pick_up_time" class="block text-sm font-medium leading-6 text-gray-900">Time</label>
+                        <input disabled value="{{ $transaction->pick_up_time }}" type="time" name="pick_up_time" id="pick_up_time" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Time">
+                    </div>
+                </div>
+                <h1 class="text-2xl font-bold my-8">Drop Off Info</h1>
+                <div class="grid gap-x-12 gap-y-3 grid-cols-2 grid-rows-2">
+                    <div>
+                        <label for="drop_off_date" class="block text-sm font-medium leading-6 text-gray-900">Drop off Date</label>
+                        <input disabled value="{{ $transaction->drop_off_date }}" type="date" name="drop_off_date" id="drop_off_date" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Date">
+                    </div>
+                    <div>
+                        <label for="Drop off Location" class="block text-sm font-medium leading-6 text-gray-900">Drop off Location</label>
+                        <input disabled value="{{ $transaction->drop_off_location }}" type="text" name="drop_off_location" id="drop_off_location" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Location">
+                    </div>
+                    <div>
+                        <label for="drop_off_time" class="block text-sm font-medium leading-6 text-gray-900">Time</label>
+                        <input disabled value="{{ $transaction->drop_off_time }}" type="time" name="drop_off_time" id="drop_off_time" class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Time">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button id="pay-button" class="flex bg-indigo-600 hover:bg-indigo-700 text-white font-bold ml-4 mt-2 mb-8 py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            Pay Now
+            <div class="ml-2">
+                <img src="../assets/ArrowRight.svg" alt="">
+            </div>
+        </button>
     <script type="text/javascript">
-        // For example trigger on button clicked, or any time you need
-        let payButton = document.getElementById('pay-button');
-        payButton.addEventListener('click', function () {
-          // Trigger snap popup. @TODO: Replace TRANSACTION_TOKEN_HERE with your transaction token.
-          // Also, use the embedId that you defined in the div above, here.
-          window.snap.pay('{{ $snapToken }}', {
-            onSuccess: function(result){
-              /* You may add your own implementation here */
-              alert("payment success!"); console.log(result);
-            },
-            onPending: function(result){
-              /* You may add your own implementation here */
-              alert("wating your payment!"); console.log(result);
-            },
-            onError: function(result){
-              /* You may add your own implementation here */
-              alert("payment failed!"); console.log(result);
-            },
-            onClose: function(){
-              /* You may add your own implementation here */
-              alert('you closed the popup without finishing the payment');
-            }
-          });
+      // For example trigger on button clicked, or any time you need
+      let payButton = document.getElementById('pay-button');
+      payButton.addEventListener('click', function () {
+        // Trigger snap popup. @TODO: Replace TRANSACTION_TOKEN_HERE with your transaction token.
+        // Also, use the embedId that you defined in the div above, here.
+        window.snap.pay('{{ $snapToken }}', {
+          onSuccess: function(result){
+            /* You may add your own implementation here */
+            alert("payment success!"); console.log(result);
+          },
+          onPending: function(result){
+            /* You may add your own implementation here */
+            alert("wating your payment!"); console.log(result);
+          },
+          onError: function(result){
+            /* You may add your own implementation here */
+            alert("payment failed!"); console.log(result);
+          },
+          onClose: function(){
+            /* You may add your own implementation here */
+            alert('you closed the popup without finishing the payment');
+          }
         });
-      </script>
+      });
+    </script>
 </body>
-{{-- <script type="text/javascript">
-    let payButton = document.getElementById('pay-button');
-    payButton.addEventListener('click', function () {
-      console.log("button clicked duar");
-
-      window.snap.pay({{ $snapToken }}, {
-            embedId: 'snap-container',
-            onSuccess: function (result) {
-              /* You may add your own implementation here */
-              alert("payment success!"); console.log(result);
-            },
-            onPending: function (result) {
-              /* You may add your own implementation here */
-              alert("wating your payment!"); console.log(result);
-            },
-            onError: function (result) {
-              /* You may add your own implementation here */
-              alert("payment failed!"); console.log(result);
-            },
-            onClose: function () {
-              /* You may add your own implementation here */
-              alert('you closed the popup without finishing the payment');
-            }
-          });
-    });
-</script> --}}
 </html>
